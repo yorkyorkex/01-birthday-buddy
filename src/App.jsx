a4e4165019a92e5cb0a35f1b1e199835d26b33ad
York Yu <n11453559@gmail.com>
2025-09-08 20:25:01 +1000

    feat: implement context for managing state in App and Person components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, createContext, useContext } from 'react'
 import data from './data'
 import List from './List'
+
+export const AppContext = createContext()
+export const useAppContext = () => useContext(AppContext)
 
 const App = () => {
   const [people, setPeople] = useState(data)
@@ -9,8 +12,9 @@ const App = () => {
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
-        <List people={people} />
-
+        <AppContext.Provider value={{ people, setPeople }}>
+          <List people={people} />
+        </AppContext.Provider>
         {people.length > 0 ? (
           <button
             type="button"
--- src/Person.jsx
+++ src/Person.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useAppContext } from './App'
 const Person = ({ id, name, age, image }) => {
+  const { people, setPeople } = useAppContext()
   return (
     <article className="person" key={id}>
       <img src={image} alt={name} className="img" />
@@ -8,6 +9,13 @@ const Person = ({ id, name, age, image }) => {
         <h4>{name}</h4>
         <p>{age} years</p>
       </div>
+      <button
+        type="button"
+        className="btn  btn-block"
+        onClick={() => setPeople(people.filter((person) => person.id !== id))}
+      >
+        Remove
+      </button>
     </article>
   )
 }
